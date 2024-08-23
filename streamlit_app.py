e6b6e3a7da98bffa99e16d8f3514bd3533227731
Kamalesh1272 <145538934+Kamalesh1272@users.noreply.github.com>
2024-08-23 14:07:00 +0530

    Update streamlit_app.py
    finding the fruits not on fruityvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,9 +15,10 @@ st.write("The name on your Smoothie order will be:", name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-# st.dataframe(data=my_dataframe, use_container_width=True)
-
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop
+                                                                                            
 Ingredients_list = st.multiselect(
     'Choose upto 5 ingredients:',
      my_dataframe,
